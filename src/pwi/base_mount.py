a104204c016a859f574cdc45bff29cefaf58b06e
michealroberts <michael@observerly.com>
2025-03-25 12:57:57 +0000

    feat: add BaseMountAlignmentMode enum to pwi module
    feat: add BaseMountAlignmentMode enum to pwi module

---
+++ src/pwi/base_mount.py
@@ -0,0 +1,26 @@
+# **************************************************************************************
+
+# @package        pwi
+# @license        MIT License Copyright (c) 2025 Michael J. Roberts
+
+# **************************************************************************************
+
+from enum import Enum
+
+# **************************************************************************************
+
+
+class BaseMountAlignmentMode(Enum):
+    """
+    Enumeration of possible mount alignment modes.
+    """
+
+    UNKNOWN = "unknown"
+    EQUATORIAL = "equatorial"
+    HORIZONTAL = "horizontal"
+    ALT_AZ = "alt_az"
+    POLAR = "polar"
+    GERMAN_POLAR = "german_polar"
+
+
+# **************************************************************************************
